5ad7d524340f690f6835f6521852ac8b3a3a0bca
C00258296(Edward Omorusi) <113979883+EdwardOmOrusi@users.noreply.github.com>
2022-10-16 22:45:52 +0100

    Update lab2bq1.java

--- Lab2b/src/labB/lab2bq1.java
+++ Lab2b/src/labB/lab2bq1.java
@@ -5,13 +5,15 @@ public class lab2bq1 {
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method 
-
+                 
+		// creating a object called rectangle One
 		Rectangle rectangleOne = new Rectangle ();
 		
+		// addding the values to each variable
 		rectangleOne.setLenght(5);
 		rectangleOne.setWidth(15);
 		
-		
+		// printing the width and lenght  of rectangle one
 		System.out.println( rectangleOne.toString());
 		
 		
