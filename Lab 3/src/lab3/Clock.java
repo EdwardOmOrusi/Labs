f5820f1a6a12f9fc6963741cbb858c15c29892f8
C00258296(Edward Omorusi) <113979883+EdwardOmOrusi@users.noreply.github.com>
2022-10-23 22:49:00 +0100

    Update Clock.java

--- Lab 3/src/lab3/Clock.java
+++ Lab 3/src/lab3/Clock.java
@@ -10,10 +10,11 @@ public static void main(String[] args)
 				// TODO Auto-generated method stub
 				 int seconds = 0 ;
 				
+	                       // setting the objects in calender and time
 				Calendar cal = Calendar.getInstance();
 				Time t = new Time(cal.get(Calendar.HOUR_OF_DAY),cal.get(Calendar.MINUTE));
 				
-				
+				// while loop if current time is biggest tha 10000
 				while(true)
 				if(System.currentTimeMillis() - seconds > 10000)
 				{
@@ -21,7 +22,7 @@ public static void main(String[] args)
 					System.out.println(System.currentTimeMillis());
 					System.out.println(t.toString());
 				
-				
+				// if get a minute is equal to 0 will break
 				if(t.getMinute() == 00)
 				{
 					break;
