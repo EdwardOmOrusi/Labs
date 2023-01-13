1e9fc7e1d94d09fbf19f0b17de985ac4641dd83b
Edward <Edward@DESKTOP-TTGF5I0.station>
2023-01-13 23:27:56 +0000

    Lab 11

---
+++ Lab11/src/lab11/carlow/ConvertTemp.java
@@ -0,0 +1,79 @@
+package lab11.carlow;
+
+//Converts a Fahrenheit temperature entered by the user to Celsius, or vice versa
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class ConvertTemp extends JFrame {
+	
+	private JTextField fahrenField = new JTextField();
+	private JTextField celsiusField = new JTextField();
+	
+	// Constructor
+	public ConvertTemp(String title)	{
+		// Set title for frame and choose layout
+		super(title);
+		getContentPane().setLayout(new GridLayout(2, 2));
+
+		// Add Fahrenheit label and text field to frame
+		add(new JLabel("Fahrenheit"));
+		add(fahrenField);
+		// Attach listener to text field
+
+		// Add Celsius label and text field to frame
+		add(new JLabel("Celsius"));
+		add(celsiusField);
+		// Attach listener to text field
+		
+		
+		fahrenField.addActionListener( new ActionListener()
+			{
+
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
+					
+					String text = fahrenField.getText();
+					double number =  Double.parseDouble(text);
+					double answer = (number - 32) * 5/9 ; 
+					celsiusField.setText(String.valueOf(answer));
+					
+					
+				}
+				
+	
+			});
+		
+		
+		celsiusField.addActionListener(new ActionListener()
+		
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				String text = celsiusField.getText();
+				double num = Double.parseDouble(text);
+				
+			}
+			
+         }
+			);
+
+		// Attach window listener
+		addWindowListener(new WindowCloser());
+	}//end costructor
+
+
+	// Listener for window
+	class WindowCloser extends WindowAdapter {
+		public void windowClosing(WindowEvent evt) {
+			System.exit(0);
+		}
+	}
+
+}//end class
